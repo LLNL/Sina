31a091e80ea7ea44fc9678501c466b95837eec12
Rebecca Haluska <haluska2@llnl.gov>
2018-11-09 14:06:00 -0800

    SIBO-407: Add database build scripts to all examples

---
+++ examples/fukushima/build_db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Needs to be used while Sina is available; see README.md
+
+set -e
+mkdir temp
+tar -C temp -xzf ../raw_data/fukushima.tgz
+python fukushima_csv2mnoda.py temp/data/AMS\ C12\ Sea\ Data.csv .
+sina ingest files/AMS_C12_SeaData.json -d data.sqlite
+rm -rf temp
---
+++ examples/noaa/build_db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Needs to be used while Sina is available; see README.md
+
+set -e
+mkdir temp
+tar -C temp -xzf ../raw_data/noaa.tar.gz
+python noaa_csv2mnoda.py temp/0123467/2.2/data/1-data/WCOA11-01-06-2015_data.csv .
+sina ingest files/WCOA11-01-06-2015.json -d data.sqlite
+rm -rf temp
