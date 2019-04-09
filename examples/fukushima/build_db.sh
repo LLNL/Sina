04ea481a96e9b53bd5e122119b4cce54dac54969
Rebecca Haluska <haluska2@llnl.gov>
2019-04-09 10:45:14 -0700

    SIBO-703: Make build_db scripts re-runnable.

--- examples/fukushima/build_db.sh
+++ examples/fukushima/build_db.sh
@@ -2,8 +2,9 @@
 # Needs to be used while Sina is available; see README.md
 
 set -e
-mkdir temp
+rm -rf temp && mkdir temp
 tar -C temp -xzf ../raw_data/fukushima.tgz
+rm -rf files && rm -f data.sqlite
 python fukushima_csv2mnoda.py --show-status temp/data/AMS\ C12\ Sea\ Data.csv .
 sina ingest files/AMS_C12_SeaData.json -d data.sqlite
 rm -rf temp
