0c7a7ddac7913f4987ec467e3fa338ebe3e21497
Zeke Morton <morton30@llnl.gov>
2020-09-08 15:12:44 -0700

    Style fixes to pass all tests

--- python/sina/datastore.py
+++ python/sina/datastore.py
@@ -179,11 +179,10 @@ class DataStore(object):
             """
             Return a list of all the data labels for data of a given type.
             Defaults to getting all data names for all records.
-            ...
+
             :param record_type: Type of records to get data names for.
             :param data_types: A list of data types to get the data names for.
-                               Current options are limited to:
-                               'scalar', 'string', 'scalar_list', 'string_list'.
+
             :returns: A generator of data names.
             """
             return self.record_dao.data_names(record_type, data_types)
