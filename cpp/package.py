222c51ec8d4a1aa33ad8fd3dac627532d5dc716c
Rebecca Haluska <haluska2@llnl.gov>
2020-09-18 16:30:46 -0700

    SIBO-937: fix adiak build, make tests default to false, other build improvements

--- cpp/package.py
+++ cpp/package.py
@@ -14,12 +14,12 @@ class SinaCpp(CMakePackage):
             description='Allow generating documentation')
     variant('adiak', default=False,
             description='Create interface for calling Sina through Adiak')
-    variant('test', default=True,
+    variant('test', default=False,
             description='Build tests')
 
     # Higher versions of cmake require C++14 or newer
     depends_on('cmake@3.8.0:', type='build')
-    depends_on('adiak', when='+adiak')
+    depends_on('adiak ~mpi', when='+adiak')
     depends_on('doxygen', type='build', when='+docs')
     depends_on('conduit ~python ~mpi ~hdf5 ~silo ~shared ~test ~hdf5_compat')
 
@@ -27,5 +27,6 @@ class SinaCpp(CMakePackage):
         spec = self.spec if self.spec is not None else ""
         return [
             '-DSINA_BUILD_ADIAK_BINDINGS={0}'.format('YES' if '+adiak' in spec else 'NO'),
-            '-DSINA_BUILD_TESTS={0}'.format('NO' if '-test' in spec else 'YES'),
+            '-DSINA_BUILD_TESTS={0}'.format('YES' if '+test' in spec else 'NO'),
+            '-DSINA_BUILD_DOCS={0}'.format('YES' if '+docs' in spec else 'NO'),
         ]
