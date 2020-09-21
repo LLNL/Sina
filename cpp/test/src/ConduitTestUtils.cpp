8b591ace811e540f92a41395f5017485f7ec99c5
Esteban Pauli <pauli2@llnl.gov>
2020-09-21 16:55:53 -0700

    SIBO-945 improved documentation

--- cpp/test/src/ConduitTestUtils.cpp
+++ cpp/test/src/ConduitTestUtils.cpp
@@ -3,6 +3,9 @@
 namespace sina { namespace testing {
 
 conduit::Node parseJsonValue(std::string const &valueAsString) {
+    // If we just try to do node.parse(valueAsString, "json"), then passing
+    // in strings does not work. We need to create a document with a key
+    // so that valueAsString can be parsed as a value.
     conduit::Node node;
     std::string fullContents = "{\"TEST_KEY\": ";
     fullContents += valueAsString;
