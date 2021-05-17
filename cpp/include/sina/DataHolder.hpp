c7b9d5b436fe01d0f2a3a327a86f985ac4df3fa7
Rebecca Haluska <haluska2@llnl.gov>
2021-05-17 10:54:27 -0700

    SIBO-966: Add library_data support to C++

---
+++ cpp/include/sina/DataHolder.hpp
@@ -0,0 +1,110 @@
+#ifndef SINA_DATAHOLDER_HPP
+#define SINA_DATAHOLDER_HPP
+
+#include <string>
+#include <unordered_map>
+
+#include "conduit.hpp"
+
+#include "sina/Datum.hpp"
+#include "sina/CurveSet.hpp"
+
+namespace sina {
+
+/**
+ * A DataHolder is a basic container for certain types of information.
+ *
+ * DataHolders contain curves, libraries, and/or data (\see Datum), and represent
+ * all the information a library can have associated with it. Records expand
+ * on DataHolders to contain additional info.
+ *
+ * \see Record
+ * \see LibraryData
+ */
+class DataHolder {
+public:
+    using DatumMap = std::unordered_map<std::string, Datum>;
+    using CurveSetMap = std::unordered_map<std::string, CurveSet>;
+    using LibraryDatumMap = std::unordered_map<std::string, DataHolder>;
+
+    /**
+     * Construct an empty DataHolder.
+     */
+    DataHolder(){}
+
+    /**
+     * Construct a DataHolder from its conduit Node representation.
+     *
+     * @param asNode the DataHolder as a Node
+     */
+    explicit DataHolder(conduit::Node const &asNode);
+
+    /**
+     * Get the DataHolder's data.
+     *
+     * @return the DataHolder's data
+     */
+    DatumMap const &getData() const noexcept {
+        return data;
+    }
+
+    /**
+     * Add a Datum to this DataHolder.
+     *
+     * @param name the key for the Datum to add
+     * @param datum the Datum to add
+     */
+    void add(std::string name, Datum datum);
+
+    /**
+     * Add a CurveSet to this DataHolder.
+     *
+     * @param curveSet the CurveSet to add
+     */
+    void add(CurveSet curveSet);
+
+    /**
+     * Get the curve sets associated with this DataHolder.
+     *
+     * @return the dataholder's curve sets
+     */
+    CurveSetMap const &getCurveSets() const noexcept {
+        return curveSets;
+    }
+
+    // TODO: should there be an add() for a completed LibraryData?
+
+    /**
+     * Add a new library to this DataHolder.
+     *
+     * @return a pointer to a new DataHolder for a library
+     * of the given name.
+     */
+    DataHolder &add_library_data(std::string const &name);
+
+    /**
+     * Get the library data associated with this DataHolder.
+     *
+     * @return the dataholder's library data
+     */
+    LibraryDatumMap const &getLibraryData() const noexcept {
+        return libraryData;
+    }
+
+    /**
+     * Convert this DataHolder to its conduit Node representation.
+     *
+     * @return the Node representation of this DataHolder.
+     */
+    conduit::Node toNode() const;
+
+
+private:
+    CurveSetMap curveSets;
+    DatumMap data;
+    LibraryDatumMap libraryData;
+};
+
+}
+
+#endif //SINA_DATAHOLDER_HPP
--- cpp/src/Record.cpp
+++ cpp/src/Record.cpp
@@ -7,15 +7,13 @@
 
 #include "sina/CppBridge.hpp"
 #include "sina/ConduitUtil.hpp"
+#include "sina/DataHolder.hpp"
 #include "sina/Run.hpp"
-#include "sina/Datum.hpp"
 
 namespace {
 
 char const LOCAL_ID_FIELD[] = "local_id";
 char const GLOBAL_ID_FIELD[] = "id";
-char const DATA_FIELD[] = "data";
-char const CURVE_SETS_FIELD[] = "curve_sets";
 char const TYPE_FIELD[] = "type";
 char const FILES_FIELD[] = "files";
 char const USER_DEFINED_FIELD[] = "user_defined";
@@ -29,7 +27,7 @@ Record::Record(ID id_, std::string type_) :
         type{std::move(type_)} {}
 
 conduit::Node Record::toNode() const {
-    conduit::Node asNode;
+    conduit::Node asNode = DataHolder::toNode();
     asNode[TYPE_FIELD] = type;
     id.addTo(asNode);
     // Optional fields
@@ -41,21 +39,6 @@ conduit::Node Record::toNode() const {
       asNode[FILES_FIELD] = fileRef;
       }
     }
-    if(!data.empty()){
-      //Loop through vector of data and append Json
-      conduit::Node datumRef;
-      for(auto &datum : data){
-          datumRef.add_child(datum.first) = datum.second.toNode();
-      }
-      asNode[DATA_FIELD] = datumRef;
-    }
-    if(!curveSets.empty()){
-      conduit::Node curveSetsNode;
-      for(auto &entry : curveSets){
-          curveSetsNode.add_child(entry.first) = entry.second.toNode();
-      }
-      asNode[CURVE_SETS_FIELD] = curveSetsNode;
-    }
     if(!userDefined.dtype().is_empty()){
       asNode[USER_DEFINED_FIELD] = userDefined;
     }
@@ -63,30 +46,15 @@ conduit::Node Record::toNode() const {
 }
 
 Record::Record(conduit::Node const &asNode) :
+        DataHolder{asNode},
         id{asNode, LOCAL_ID_FIELD, GLOBAL_ID_FIELD},
         type{getRequiredString(TYPE_FIELD, asNode, "record")} {
-    if(asNode.has_child(DATA_FIELD)){
-        auto dataIter = asNode[DATA_FIELD].children();
-        //Loop through DATA_FIELD objects and add them to data:
-        while(dataIter.has_next()){
-            auto &namedDatum = dataIter.next();
-            data.emplace(std::make_pair(dataIter.name(), Datum(namedDatum)));
-        }
-    }
+
     if(asNode.has_child(FILES_FIELD)){
         auto filesIter = asNode[FILES_FIELD].children();
         while(filesIter.has_next()){
             auto &namedFile = filesIter.next();
             files.insert(File(filesIter.name(), namedFile));
-        }
-    }
-    if (asNode.has_child(CURVE_SETS_FIELD)) {
-        auto curveSetsIter = asNode[CURVE_SETS_FIELD].children();
-        while(curveSetsIter.has_next()){
-            auto &curveSetNode = curveSetsIter.next();
-            std::string name = curveSetsIter.name();
-            CurveSet cs{name, curveSetNode};
-            curveSets.emplace(std::make_pair(std::move(name), std::move(cs)));
         }
     }
     if(asNode.has_child(USER_DEFINED_FIELD)){
@@ -101,30 +69,12 @@ Record::Record(conduit::Node const &asNode) :
     }
 }
 
-void Record::add(std::string name, Datum datum) {
-    auto existing = data.find(name);
-    if (existing == data.end()) {
-        data.emplace(std::make_pair(name, datum));
-    } else {
-        existing->second = datum;
-    }
-}
-
 void Record::add(File file) {
     auto existing = files.find(file);
     if (existing != files.end()) {
         files.erase(existing);
     }
     files.insert(std::move(file));
-}
-void Record::add(CurveSet curveSet) {
-    auto name = curveSet.getName();
-    auto existing = curveSets.find(name);
-    if (existing == curveSets.end()) {
-        curveSets.emplace(name, std::move(curveSet));
-    } else {
-        existing->second = std::move(curveSet);
-    }
 }
 
 void Record::setUserDefinedContent(conduit::Node userDefined_) {
